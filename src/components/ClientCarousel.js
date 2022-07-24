79ccf098bd634a81c18e42fc115757f673c63d66
Ranjananeutroline <Ranjana.neutroline@gmail.com>
2022-07-24 16:15:16 +0545

    first commit

--- src/components/ClientCarousel.js
+++ src/components/ClientCarousel.js
@@ -7,84 +7,118 @@ import Col from 'react-bootstrap/Col';
 
 export default function ClientCarousel() {
   return (
-    <div className='container'>
+    <div className='container p-3'>
        <Carousel>
       <Carousel.Item>
-      <Row className='client-section p-2'>
-        <Col sm={4}>
-        <img
-          className="client-img"
-          style={{height:200}}
-          src="client2.jpg"
-          alt="First slide"
-        />
-        <div className='client-caption'>
-        <h5>Client 1</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt</p>
-        </div>
+      <Row className='client-section p-5'>
+        <Col sm={4} className='client-column p-3'>
+            <div className='client-image p-2'>
+            <img
+              style={{height:200}}
+              src="client2.jpg"
+              alt="First slide"
+            />
+            </div>
+            
+            <div className='client-caption p-2'>
+            <h5>Client 1</h5>
+            <div className='client-p p-3'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt</p>
+            </div>
+            </div>
         
         </Col>
-        <Col sm={4}>
-        <img
-          className="client-img"
-          style={{height:200}}
-          src="client3.jpg"
-          alt="First slide"
-        />
-        <h5>Client 1</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt</p>
+        <Col sm={4} className='client-column p-3'>
+            <div className='client-image p-2'>
+            <img
+              style={{height:200}}
+              src="client3.jpg"
+              alt="First slide"
+            />
+            </div>
+
+            <div className='client-caption p-2'>
+            <h5>Client 2</h5>
+            <div className='client-p p-3'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt</p>
+            </div>
+            </div>
         </Col>
-        <Col sm={4}>
-        <img
-          className="client-img"
-          style={{height:200}}
-          src="client4.jpg"
-          alt="First slide"
-        />
-        <h5>Client 1</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt</p>
+        <Col sm={4} className='client-column p-3'>
+        <div className='client-image p-2'>
+            <img
+              style={{height:200}}
+              src="client4.jpg"
+              alt="First slide"
+            />
+            </div>
+
+            <div className='client-caption p-2'>
+            <h5>Client 3</h5>
+            <div className='client-p p-3'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt</p>
+            </div>
+            </div>
         </Col>
       </Row>
         
         
       </Carousel.Item>
       <Carousel.Item interval={500}>
-      <Row className='client-section'>
-        <Col sm={4}>
-        <img
-          className="client-img"
-          style={{height:200}}
-          src="client4.jpg"
-          alt="First slide"
-        />
-        <h5>Client 1</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt</p>
+      <Row className='client-section p-5'>
+        <Col sm={4} className='client-column p-3'>
+        <div className='client-image p-2'>
+            <img
+              style={{height:200}}
+              src="client4.jpg"
+              alt="First slide"
+            />
+            </div>
+
+            <div className='client-caption p-2'>
+            <h5>Client 4</h5>
+            <div className='client-p p-3'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt</p>
+            </div>
+            </div>
         </Col>
-        <Col sm={4}>
-        <img
-          className="client-img"
-          style={{height:200}}
-          src="client3.jpg"
-          alt="First slide"
-        />
-        <h5>Client 1</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt</p>
+        <Col sm={4} className='client-column p-3'>
+        <div className='client-image p-2'>
+            <img
+              style={{height:200}}
+              src="client3.jpg"
+              alt="First slide"
+            />
+            </div>
+
+            <div className='client-caption p-2'>
+            <h5>Client 5</h5>
+            <div className='client-p p-3'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt</p>
+            </div>
+            </div>
         </Col>
-        <Col sm={4}>
-        <img
-          className="client-img"
-          style={{height:200}}
-          src="client2.jpg"
-          alt="First slide"
-        />
-        <h5>Client 1</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt</p>
+        <Col sm={4} className='client-column p-3'>
+        <div className='client-image p-2'>
+            <img
+              style={{height:200}}
+              src="client2.jpg"
+              alt="First slide"
+            />
+            </div>
+
+            <div className='client-caption p-2'>
+            <h5>Client 6</h5>
+            <div className='client-p p-3'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt</p>
+            </div>
+            </div>
         </Col>
       </Row>
       </Carousel.Item>
