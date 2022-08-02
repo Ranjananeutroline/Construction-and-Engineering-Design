22ce9d6ed6b6671e43605d0be4cd1dbde37586bf
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-08-03 04:24:53 +0545

    feat:navitems background and navabr size reduced

--- src/components/home/Navbar/NavLinkProps.jsx
+++ src/components/home/Navbar/NavLinkProps.jsx
@@ -5,7 +5,8 @@ import { LinkContainer } from 'react-router-bootstrap'
 function NavLinkProps({link,name}) {
   return (
     <>
-       <LinkContainer to={link} style={{ paddingRight: "40px" }}>
+       <LinkContainer to={link}>
+       {/* style={{ paddingRight: "40px" }} */}
             <Nav.Link >{name}</Nav.Link>
           </LinkContainer>{" "}
     </>
